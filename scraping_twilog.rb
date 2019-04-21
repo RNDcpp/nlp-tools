7de8fe264f0a52442767fc720fb9759069e3db72
RNDcpp <yonbanki@gmail.com>
2019-04-21 23:45:03 +0900

    TwilogParser.getwordsの条件をパラメータ化

--- scraping_twilog.rb
+++ scraping_twilog.rb
@@ -3,7 +3,6 @@ require 'open-uri'
 require 'nokogiri'
 require 'natto'
 require 'sqlite3'
-#db = SQLite3::Database.new("../db/#{ARGV[0]}.db")
 module HTMLParser
   class << self
     def check_url(url)
@@ -12,7 +11,6 @@ module HTMLParser
         url=URI.encode(url)
         url=URI.parse(url)
         open(url,'User-Agent'=>'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)') 
-        #open("http://twilog.org/patioglass/1") 
       rescue => e
         puts "#{e.backtrace}"
         puts "#{e.class.name}:#{e.message}"
@@ -41,17 +39,22 @@ module TwilogParser
       end
       return posts
     end
-    def get_words(screen_name,limit)
+    def get_words(screen_name,limit,features=["名詞","形容詞","動詞","形容動詞"],permit_length=8)
       nat = Natto::MeCab.new
+      permit_features=%r"(#{features.join("|")})"
       get_status_list(screen_name,limit) do |ary,txt|
-        txt.gsub!(/@\w+/,"")
-        txt.gsub!(/(http|https):\/\/([\w]+\.)+[\w]+(\/[\w\.\/?%&=]*)?/,"")
+        txt.gsub!(%r"@\w+","")
+        txt.gsub!(%r"(http|https):\/\/([\w]+\.)+[\w]+(\/[\w\.\/?%&=]*)?","")
         puts txt
         nat.parse(txt) do |word|
-          #if (word.surface =~ /\S+/)and (word.surface !~ /(@.*|_.*|\..*|\/.*|:.*|\(.*|\/.*|\).*|#.*|%.*|\s)/)and((word.surface.length != 1)or(word.surface !~ /[0-9]|[a-z]|[A-Z]|[あ-ん]|[ア-ン]/)) and (word.feature =~ /(名詞|形容詞|動詞|形容動詞)/) and word.surface.length < 8
-              puts word.surface
-              ary << word.surface
-          #end
+          if (word.surface =~ %r"\S+") &&\
+            (word.surface !~ %r"(@.*|_.*|\..*|\/.*|:.*|\(.*|\/.*|\).*|#.*|%.*|\s)" &&\
+            ((word.surface.length != 1)or(word.surface !~ %r"[0-9]|[a-z]|[A-Z]|[あ-ん]|[ア-ン]")) &&\
+            (word.feature =~ permit_features) &&\
+            word.surface.length < permit_length
+            puts word.surface
+            ary << word.surface
+          end
         end
       end
     end
@@ -63,20 +66,3 @@ end
 TwilogParser.get_words(ARGV[0],ARGV[1].to_i).each do |t|
   puts t
 end
-#end
-#words.each do |word,num|
-#  puts "#{word}:#{words[word]}"
-#  {
-#  "#{word}" => num,
-#  }.each do |pair|
-#    db.execute "insert into pword values ( ?, ? )", pair
-#  end
-#end
-#db.execute "insert into wcount values (?)",wc
-#db.execute( "select * from pword order by num asc" ) do |row|
-#  p row
-#end
-#db.execute( "select * from wcount" ) do |row|
-#  p row
-#end
-
